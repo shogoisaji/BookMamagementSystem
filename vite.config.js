f9383cd76e9b446288007709c786fd08b0018e16
shogoisaji <simacsimac9@gmail.com>
2023-10-10 20:28:15 +0900

    ggggg

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    envPrefix: 'VITE_',
-
-    build: {
-    minify: 'terser',
-    terserOptions: {
-        compress: {
-        drop_console: true,
-        }
-    },
-
-    rollupOptions: {
-        onwarn: (warning) => {
-        console.error(warning.message);
-        }
-    }
-    }
 });
