f50d3eb035c37e347c951cf6c9994a5e085d0b63
shogoisaji <simacsimac9@gmail.com>
2023-10-11 23:04:48 +0900

    sssssssss

--- resources/views/components/login-layout.blade.php
+++ resources/views/components/login-layout.blade.php
@@ -12,7 +12,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <!-- Styles -->
+        <link rel="stylesheet" href="/build/assets/app.css">
+
+        <!-- Scripts -->
+        <script src="/build/assets/app.js"></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-emerald-200">
